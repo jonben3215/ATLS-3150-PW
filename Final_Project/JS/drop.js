1a060dba7c41583584e0e48ba907b0bd10d77b8d
Ben Xiang <63603790+jonben3215@users.noreply.github.com>
2023-12-06 19:41:28 -0700

    updated demo

--- Final_Project/JS/drop.js
+++ Final_Project/JS/drop.js
@@ -1,12 +1,16 @@
 // Function to toggle the dropdown content visibility
 function toggleDropdown(dropdownId) {
-    var dropdownContent = document.getElementById(dropdownId);
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  }  
+  var dropdownContent = document.getElementById(dropdownId);
+  var dropdownIcon = document.getElementById("dropdownIcon");
+
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+    dropdownIcon.innerHTML = "&#9658;"; // Change to ">"
+  } else {
+    dropdownContent.style.display = "block";
+    dropdownIcon.innerHTML = "&#9660;"; // Change to "V"
+  }
+} 
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropdown button')) {
