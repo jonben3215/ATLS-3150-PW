d513626003986ac10f3b54fddfd08ca9b74b15bc
Ben Xiang <63603790+jonben3215@users.noreply.github.com>
2023-12-09 15:18:04 -0700

    Moved the js

--- Final_Project/img/JS/demo.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var myButton = document.getElementById("Demo");
-    myButton.addEventListener("click", function() {
-        window.open("https://www.figma.com/proto/T7xEpwxdN5bHkfovQrjGIt/ParkPro?node-id=1-2");
-    });
-});
